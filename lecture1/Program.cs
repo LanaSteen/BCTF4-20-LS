07d769636fd1a223aab8a67a6d1fc5e342c003f3
LanaNew <l4n.steen@gmail.com>
2026-05-05 23:38:03 +0400

    lecture 1

--- lecture1/Program.cs
+++ lecture1/Program.cs
@@ -6,10 +6,49 @@
 		{
 
 
+			//'K'  'e'   ''  
 
-			Console.WriteLine("Hello, World!");
+			//'1'  '@'   16 
+
+			char symbol = 't';
+			int num1;
+			num1 = 10;
+
+
+			string str = "hello";
+
+			//"hello"  
+
+
+
+
+
+			Console.WriteLine(symbol); // გამომტანი    // t ascii table 116
+			Console.ReadLine();  // შემომტანი
+
+
+
+			//cw
+			
+			Console.WriteLine();
 
 
 		}
 	}
 }
+
+/*  login part
+ 
+ფუნქცია
+მეთოდი - ჩაშენებული
+
+
+*/
+
+
+
+#region  register
+
+
+
+#endregion  register
