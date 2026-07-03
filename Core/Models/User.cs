1d88006742b78f6df3d79b024734a9580f5d7b70
LanaNew <l4n.steen@gmail.com>
2026-07-03 21:04:55 +0400

    dll

---
+++ Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+	public class User
+	{
+		public static void Test()
+		{
+			Console.WriteLine("hello2");
+		}
+	}
+}
---
+++ UI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+
+namespace UI
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.ReadLine();
+
+			User.Test();
+
+			Console.WriteLine("Hello World!");
+
+			Console.ReadLine();
+		}
+	}
+}
