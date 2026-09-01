ff3f49e27d659245f06177ac8ccdd136389c4532
LanaNew <l4n.steen@gmail.com>
2026-09-01 22:28:06 +0400

    ado.net

---
+++ 1ADONet/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace _1ADONet
+{
+
+	internal class Program
+	{
+
+		private static readonly string _connectionString = "Data Source=LAPTOP-HE9JLDVE; Database=UNIVERSITY; Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\";Command Timeout=0";
+
+
+		static void Main(string[] args)
+		{
+
+			Console.InputEncoding = Encoding.UTF8;
+			Console.OutputEncoding = Encoding.UTF8;
+			ReadStudentsAllData();
+		}
+
+
+		static void ReadStudentsAllData()
+		{
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				connection.Open();
+				string query = "SELECT * FROM Students";
+				using (SqlCommand command = new SqlCommand(query, connection))
+				{
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							if (reader.HasRows)
+							{
+								//int id = reader.GetInt32("ID");
+								//string name = reader.GetString("Name");
+
+								int id = (int)reader["ID"];
+								string name = (string)reader["Name"];
+
+								Console.WriteLine($"ID: {id}, Name: {name}");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
